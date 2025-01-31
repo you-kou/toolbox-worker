caf529a2bc514579b890402a5fb408c7d6bd0061
you-kou <1352563715@qq.com>
2025-01-31 17:53:03 +0800

    修正bug

--- src/homepage.js
+++ src/homepage.js
@@ -1,30 +1,28 @@
 import { getToolRecommendations } from './tool-recommendation';
 
 export const handleHomepageRequest = async (env) => {
-	if (method === "GET") {
-		const tools = await env.DB.prepare(
-			"SELECT t1.CategoryName, t2.SubcategoryName, t3.* " +
-			"FROM ToolCategorys t1 " +
-			"JOIN ToolSubcategorys t2 " +
-			"ON t1.CategoryId = t2.CategoryId " +
-			"JOIN ToolOverviews t2 " +
-			"ON t2.CategoryId = t3.CategoryId " +
-			"AND t2.SubcategoryId = t3.SubcategoryId " +
-			"WHERE t2.SubcategoryId = 'a'",
-		)
-			.all().results;
+	const tools = await env.DB.prepare(
+		"SELECT t1.CategoryName, t2.SubcategoryName, t3.* " +
+		"FROM ToolCategorys t1 " +
+		"JOIN ToolSubcategorys t2 " +
+		"ON t1.CategoryId = t2.CategoryId " +
+		"JOIN ToolOverviews t2 " +
+		"ON t2.CategoryId = t3.CategoryId " +
+		"AND t2.SubcategoryId = t3.SubcategoryId " +
+		"WHERE t2.SubcategoryId = 'a'",
+	)
+		.all().results;
 
-		const recommendations = await getToolRecommendations(env);
+	const recommendations = await getToolRecommendations(env);
 
-		const result = {
-			tools,
-			recommendations
-		}
-
-		return new Response(JSON.stringify(result), {
-			headers: {
-				'Content-Type': 'application/json'
-			},
-		});
+	const result = {
+		tools,
+		recommendations
 	}
+
+	return new Response(JSON.stringify(result), {
+		headers: {
+			'Content-Type': 'application/json'
+		},
+	});
 }
